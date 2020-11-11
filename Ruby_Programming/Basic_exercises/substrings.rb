5468672dff983c30550b4b31a544f94c155c4896
Riaz Mohamed <railsdevriaz@gmail.com>
2020-11-11 21:23:19 +0000

    edit substring.rb

--- Ruby_Programming/Basic_exercises/substrings.rb
+++ Ruby_Programming/Basic_exercises/substrings.rb
@@ -5,5 +5,5 @@ def substrings(string, dictionary)
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
+p substrings("below", dictionary)
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
