60ea8e42737e47719aff3b9fd091b94eb67ead6d
Riaz Mohamed <railsdevriaz@gmail.com>
2020-11-16 09:42:07 +0000

    Practice with class variables

---
+++ Ruby_Programming/OOPS/classy.rb
@@ -0,0 +1,18 @@
+class Person
+  @@people_count = 0
+
+  def initialize(name)
+    @name = name
+    @@people_count += 1
+  end
+
+  def self.people_count
+    @@people_count
+  end
+end
+
+matz = Person.new("Yukihiro")
+dhh = Person.new("David")
+ranm = Person.new("Riaz")
+
+puts "The total number of people = #{Person.people_count}"
