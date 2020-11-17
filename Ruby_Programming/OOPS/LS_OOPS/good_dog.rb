676e3cdaeb7bad4461f19462cea230f8a1800d6e
Riaz Mohamed <railsdevriaz@gmail.com>
2020-11-17 15:22:28 +0000

    add good_dog.rb

---
+++ Ruby_Programming/OOPS/LS_OOPS/good_dog.rb
@@ -0,0 +1,18 @@
+class GoodDog
+  attr_accessor :name, :age
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def details
+    puts "Name: #{@name}, Age: #{@age}."
+  end
+end
+
+sparky = GoodDog.new("Sparky", 4)
+fido = GoodDog.new("Fido", 6)
+spot = GoodDog.new("Spot", 10)
+sparky.details
+fido.details
+spot.details
