5e71bb197d04e120d193e80222a36428a7892c2f
Riaz Mohamed <railsdevriaz@gmail.com>
2020-11-18 13:17:07 +0000

    mixins.rb

---
+++ Ruby_Programming/OOPS/LS_OOPS/mixins.rb
@@ -0,0 +1,31 @@
+module Swimmable
+  def swim
+    "I'm swimming!"
+  end
+end
+
+class Animal
+
+end
+
+class Fish < Animal
+  include Swimmable
+end
+
+class Mammal < Animal
+
+end
+
+class Cat < Animal
+end
+
+class Dog < Animal
+  include Swimmable
+end
+
+sparky = Dog.new
+neemo = Fish.new
+paws = Cat.new
+
+puts sparky.swim
+puts neemo.swim
