4ccce7152c55f4370bafa1c53c17ec1816b4c242
Riaz Mohamed <railsdevriaz@gmail.com>
2020-11-18 10:23:57 +0000

    add more behaviour to my_car.rb

--- Ruby_Programming/OOPS/LS_OOPS/my_car.rb
+++ Ruby_Programming/OOPS/LS_OOPS/my_car.rb
@@ -1,5 +1,6 @@
 class MyCar
-  # attr_accessor :year, :color, :model
+  attr_accessor :color
+  attr_reader :year
   def initialize(year, model, color)
     @speed = 0
     @year = year
@@ -25,6 +26,11 @@ class MyCar
     @speed = 0
     puts "The car has stopped!"
   end
+
+  def spray_paint(color)
+    self.color = color
+    puts "The new paint on the car is #{self.color}"
+  end
 end
 
 car = MyCar.new(2018, 'MAZDA 6', 'red')
@@ -32,3 +38,11 @@ car.speed_up(70)
 car.brake(50)
 car.current_speed
 car.shut_off
+puts car.color
+car.color = "black"
+puts car.color
+puts car.year
+puts "Enter the new colour"
+puts ">....."
+new_color = gets.chomp
+car.spray_paint(new_color)
