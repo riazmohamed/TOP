e25bc8394bef29234b54e368a08a54419569b621
Riaz Mohamed <railsdevriaz@gmail.com>
2020-11-25 14:45:26 +0000

    Refreactor Gear class using Struct

---
+++ Ruby_Programming/OOPS/POODR/gear1.rb
@@ -0,0 +1,26 @@
+class Gear
+  attr_reader :chainring, :cog , :wheel
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @wheel = Wheel.new(rim, tire)
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
+  end
+end
+
+gear = Gear.new(10, 10, 10, 10)
+p gear.gear_inches
