fa9a1b3a2e4a5de9fef3b309384810303c71900a
Riaz Mohamed <railsdevriaz@gmail.com>
2020-11-25 20:09:57 +0000

    Add explicit defaults

---
+++ Ruby_Programming/OOPS/POODR/explicit_defaults.rb
@@ -0,0 +1,35 @@
+class Gear
+  attr_reader :chainring, :cog, :wheel # getter methods
+
+  def initialize(chainring: 40, cog: 18, wheel:) # keyword arguments
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
+  end
+
+  def gear_inches
+    ratio * diameter
+  end
+
+  def diameter
+    wheel.diameter
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
+
+class Wheel
+  attr_reader :rim, :tire
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+      rim + (tire * 2)
+  end
+end
+
+puts Gear.new(:wheel => Wheel.new(26, 1.5)).chainring
