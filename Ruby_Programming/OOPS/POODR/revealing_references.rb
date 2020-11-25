f8393ed0c020647b9254c8840accc5e4d9c2ba44
Riaz Mohamed <railsdevriaz@gmail.com>
2020-11-25 13:36:05 +0000

    Add POODR folder

---
+++ Ruby_Programming/OOPS/POODR/revealing_references.rb
@@ -0,0 +1,21 @@
+class RevealingReferences
+    attr_reader :wheels
+
+    def initialize(data) 
+        @wheels = wheelify(data)
+    end
+
+    def diameters
+        wheels.collect { |wheel| wheel.rim + (wheel.tire * 2) }
+    end
+
+    # now everyone can send rim/tire to the wheel 
+    Wheel = Struct.new(:rim, :tire)
+    def wheelify(data)
+        data.collect { |cell| Wheel.new(cell[0], cell[1]) } 
+    end
+end
+
+tyre = RevealingReferences.new([[1, 2], [3, 4]])
+p tyre.wheels
+p tyre.diameters
