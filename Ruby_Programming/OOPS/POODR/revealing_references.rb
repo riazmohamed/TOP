27f3ffc653385457ec17bb65789ec0dbe01445d4
Riaz Mohamed <railsdevriaz@gmail.com>
2020-11-25 14:23:56 +0000

    Refractoring

--- Ruby_Programming/OOPS/POODR/revealing_references.rb
+++ Ruby_Programming/OOPS/POODR/revealing_references.rb
@@ -1,18 +1,22 @@
 class RevealingReferences
     attr_reader :wheels
 
-    def initialize(data) 
+    def initialize(data)
         @wheels = wheelify(data)
     end
 
     def diameters
-        wheels.collect { |wheel| wheel.rim + (wheel.tire * 2) }
+        wheels.collect { |wheel| diameter(wheel) }
     end
 
-    # now everyone can send rim/tire to the wheel 
+    def diameter(wheel)
+      wheel.rim + (wheel.tire * 2)
+    end
+
+    # now everyone can send rim/tire to the wheel
     Wheel = Struct.new(:rim, :tire)
     def wheelify(data)
-        data.collect { |cell| Wheel.new(cell[0], cell[1]) } 
+        data.collect { |cell| Wheel.new(cell[0], cell[1]) }
     end
 end
 
