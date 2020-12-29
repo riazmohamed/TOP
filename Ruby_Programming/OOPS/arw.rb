f9a72d26220ccfbe11f99becede350503faf70c0
Riaz Mohamed <railsdevriaz@gmail.com>
2020-12-29 10:13:47 +0000

    ruby attribute accessor practice problem

---
+++ Ruby_Programming/OOPS/arw.rb
@@ -0,0 +1,48 @@
+class Car
+  # attr_reader :name, :price
+  # attr_writer :name, :price
+
+  def initialize
+    @name
+    @price
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def name
+    @name
+  end
+
+  def price
+    @price
+  end
+
+  def someth
+    "#{name}: #{price}"
+  end
+
+  def to_s
+    "#{name}: #{price}"
+  end
+end
+
+c1 = Car.new
+c2 = Car.new
+
+c1.name = "Porche"
+c1.price = 23500
+c2.name = "Volkswagon"
+c2.price = 9500
+
+puts "The #{c1.name} costs #{c1.price}"
+
+# the puts method automatically calls the to_s method on the caller
+puts c1
+puts c2
+# hence the above expression outputs the return value of the to_s method in class definition
