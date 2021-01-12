dbab893e32827eaf1e2cbea31190daa02a8db400
Riaz Mohamed <railsdevriaz@gmail.com>
2021-01-12 16:58:54 +0000

    Restart program

--- Ruby_Programming/OOPS/tictactoe_oops.rb
+++ Ruby_Programming/OOPS/tictactoe_oops.rb
@@ -1,58 +1,38 @@
 #tictactoe.rb
 
-module TicTacToe
+class Board
+  attr_reader :board
+  
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                   [[1, 5, 9], [3, 5, 7]]              # diagonals
 
-  puts "Player A your sign is 'X'"
-  puts "Player B your sign is 'O'"
-
-end
-
-class Board
-  attr_accessor :board_layout, :available_positions
-  include TicTacToe
-
-  def initialize(board_layout, available_positions)
-    @board_layout = board_layout
-    @available_positions = available_positions
+  def initialize
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   def display_board
     puts ""
-    puts "#{board_layout[0]} | #{board_layout[1]} | #{board_layout[2]}"
+    puts "#{board[0]} | #{board[1]} | #{board[2]}"
     puts "--+---+--"
-    puts "#{board_layout[3]} | #{board_layout[4]} | #{board_layout[5]}"
+    puts "#{board[3]} | #{board[4]} | #{board[5]}"
     puts "--+---+--"
-    puts "#{board_layout[6]} | #{board_layout[7]} | #{board_layout[8]}"
+    puts "#{board[6]} | #{board[7]} | #{board[8]}"
     puts ""
   end
 end
 
-class Prompt < Board
-  attr_accessor :player_name, :symbol
+class Display
 
-  def initialize(player_name, symbol)
-    @player_name = player_name
-    @symbol = symbol
-  end
-
-  def prompt_user
-    user_input = ""
-    puts "#{player_name}: Select your #{symbol} position:"
-    loop do
-      puts "Please choose a value from #{available_positions}"
-      user_input = gets.chomp.to_i
-      break if available_positions.include?(user_input)
-      puts "Invalid option!!! Select an Integer from #{available_positions}"
-    end
-    user_input
-  end
 end
 
-board_layout = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-available_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+class Game
 
-brd = Board.new(board_layout, available_positions)
-puts brd.display_board
+end
+
+class Player
+
+end
+
+ brd = Board.new
+ p brd.display_board
