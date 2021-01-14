7dcee2e1631b33d74f7dd24e4039970778bbeabb
Riaz Mohamed <railsdevriaz@gmail.com>
2021-01-14 10:43:13 +0000

    add mixins to tictactoe_oops.rb

--- Ruby_Programming/OOPS/tictactoe_oops.rb
+++ Ruby_Programming/OOPS/tictactoe_oops.rb
@@ -1,40 +1,57 @@
 #tictactoe.rb
 
-class Board
-  attr_reader :board
-
-  WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
-                  [[1, 5, 9], [3, 5, 7]]              # diagonals
-
-  def initialize
-    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+module Display
+  def play_intro
+    puts "Let's play TICTACTOE !!!"
   end
 
-  def display_board
-    puts ""
-    puts "#{board[0]} | #{board[1]} | #{board[2]}"
-    puts "--+---+--"
-    puts "#{board[3]} | #{board[4]} | #{board[5]}"
-    puts "--+---+--"
-    puts "#{board[6]} | #{board[7]} | #{board[8]}"
-    puts ""
+  def ask_name
+    puts "Please enter player name: "
   end
 
-  # bring in available position
-end
+  def user_input_warning
+    puts "Invalid option!!! Select an Integer from"
+  end
 
-class Display
+  class Board
+    attr_accessor :cell
 
+    WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                    [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                    [[1, 5, 9], [3, 5, 7]]              # diagonals
+
+    def initialize
+      @cell = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end
+
+    def display_cell
+      puts ""
+      puts "#{cell[0]} | #{cell[1]} | #{cell[2]}"
+      puts "--+---+--"
+      puts "#{cell[3]} | #{cell[4]} | #{cell[5]}"
+      puts "--+---+--"
+      puts "#{cell[6]} | #{cell[7]} | #{cell[8]}"
+      puts ""
+    end
+
+    # bring in available position
+  end
+
+  class Player
+
+  end
 end
 
 class Game
+  include Display
+  attr_accessor :board
+
+  def initialize
+    @board = Board.new
+  end
 
 end
 
-class Player
 
-end
-
-brd = Board.new
-p brd.display_board
+game = Game.new
+p game.board.display_cell
