3cb8f1d09f1a427c0f9da724a993ab092ccc6f1a
riazmohamed <railsdevriaz@gmail.com>
2021-04-01 19:13:20 +0100

    serialization test

--- Ruby_Programming/Serialization/event_manager/lib/event_manager.rb
+++ Ruby_Programming/Serialization/event_manager/lib/event_manager.rb
@@ -1,1 +1,18 @@
-puts EventManager Initialized!
+require 'csv'
+
+def clean_zipcode(zipcode)
+  if zipcode.length > 5
+    zipcode.slice(0, 5)
+  else
+    zipcode = ("00000" + zipcode).slice(-5, 5)
+  end
+end
+
+puts "Event Manager Initialized!"
+
+contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode].to_s)
+  puts "#{name} #{zipcode}"
+end
